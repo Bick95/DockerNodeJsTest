4ffeabb7c8fdfbabad7b2e6e27466f6813e2dea2
Bick95 <daniel.bick@live.de>
2020-09-12 18:25:43 +0200

    Continued tutorial https://www.youtube.com/watch?v=OstALBk-jTc

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,10 +2,22 @@ import Dashboard from "./views/Dashboard.js"
 import Posts from "./views/Posts.js"
 import Settings from "./views/Settings.js"
 import About from "./views/About.js"
-
-//const express = require("express");
+import PostView from "./views/PostView.js"
+import Error404 from "./views/Error404.js"
 
 console.log("JS loaded");
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        //console.log([key, values[i]]);
+        return [key, values[i]];
+    }));
+};
 
 const navigateTo = url => {
     history.pushState(null, null, url);
@@ -14,40 +26,35 @@ const navigateTo = url => {
 
 const router = async () => {
     const routes = [
-        
-            //{ path: "/", view: () => console.log("Viewing Dashboard!") },
+
             { path: "/", view: Dashboard },
+            { path: "/error404", view: Error404 },
             { path: "/posts", view: Posts },
+            { path: "/posts/:id", view: PostView },
             { path: "/settings", view: Settings },
             { path: "/about", view: About },
-            //{ path: "404", view: () => console.log("Error 404. Page not found!") },
-            //{ path: "/posts", view: () => console.log("Viewing Posts!") },
-            //{ path: "/settings", view: () => console.log("Viewing Settings!") },
-            
-
-        
     ];
 
     // Test each route for potential match 
     const potentialMatches = routes.map(route => {
         return {
             route: route,
-            isMatch: location.pathname === route.path
+            result: location.pathname.match(pathToRegex(route.path))
         };
     });
 
     //console.log(potentialMatches);
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
-
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+    
     if (!match) {
         match = {
-            route: routes[0],
-            isMatch: true
+            route: routes[1],
+            result: [location.pathname]
         };
     }
 
-    const view = new match.route.view();
+    const view = new match.route.view(getParams(match));
 
     document.querySelector("#app").innerHTML = await view.getHTML();
 
--- frontend/static/js/views/AbstractView.js
+++ frontend/static/js/views/AbstractView.js
@@ -1,6 +1,9 @@
 export default class {
-    constructor() {
-        console.log("Placeholder...");
+    constructor(params) {
+        console.log("Abstract view constructor");
+        this.params = params;
+
+        console.log("Params: " + this.params);
     }
     
     setTitle(title) {
